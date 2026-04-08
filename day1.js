4cc935088b6180d30f7bbc543a9b4fddb5aa162f
saikumar <venkatsai.terminator@gmail.com>
2026-04-08 07:19:14 +0530

    Day1 JS Fundamentals basics

---
+++ day1.js
@@ -0,0 +1,27 @@
+let name = "bangaram";
+console.log(name);
+
+let a = 10; 
+let b = 20;
+let x = a+b;
+console.log(x);
+
+//exercise 1 : 
+
+let age = 30; 
+console.log(age);
+
+let y = 100; 
+let z = 200;
+//addition
+console.log(y+z);
+
+//subtraction 
+console.log(z-y);
+
+//task 3
+
+let firstName = 'Venkat';
+let age1 = 26; 
+console.log("My name is " + firstName + " and I am " + age1);
+
