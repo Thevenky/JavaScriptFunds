014b951af362de2e51dddadc4609797164b09398
saikumar <venkatsai.terminator@gmail.com>
2026-04-12 05:53:53 +0530

    Add day2.js file

---
+++ day2.js
@@ -0,0 +1,41 @@
+//Loops
+//loops are needed to repeat the message multiple times without writing the same code again and again
+
+//for loop
+//syntax: for(initialization; condition; increment/decrement){
+//code to be executed
+//}
+
+// for (statement1; statement2; statement3){
+
+// }
+
+for (let i = 0; i < 5; i++){
+    console.log("Hello World");
+}
+
+var cars = ['BMW', 'Audi', 'Mercedes'];
+//for of loop
+//syntax: for(variable of iterable){
+//code to be executed
+//}
+
+for(let car of cars){
+    console.log(car);
+    if (car == "Audi"){
+        break; //break statement is used to exit the loop when a certain condition is met
+    }
+}
+
+//The beloe is ES6 syntax
+
+cars.forEach(car =>{
+    console.log(car);
+})
+
+
+//print numbers 1 to 5 
+
+for (i=1; i<=5; i++){
+    console.log(i)
+}
